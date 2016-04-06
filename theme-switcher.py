45d52e7022df9adedf6456f4f001c243968d2de9
Gregory <gregory.petrosyan@mail.utoronto.ca>
2016-04-06 15:34:20 -0400

    utilize sublime.find_resources()

--- theme-switcher.py
+++ theme-switcher.py
@@ -18,15 +18,10 @@ MENU_BASE = """
 ]
 """.strip()
 
-INST_PKGS_PATH = os.path.join(
-    os.path.dirname(sublime.packages_path()), "Installed Packages"
-);
-
 PKG_FOLDER = os.path.join(sublime.packages_path(), "Theme-Switcher")
 THEMES_MENU = os.path.join(PKG_FOLDER, "Main.sublime-menu")
 
 def init():   
-
     if not os.path.isdir(PKG_FOLDER):
         os.makedirs(PKG_FOLDER)
     
@@ -40,43 +35,44 @@ def init():
 
 
 def create_menu():
+    menu=[]
+    d={}
 
-    menu = []
-    packages = sorted(list(os.listdir(INST_PKGS_PATH)))
+    for theme_path in sublime.find_resources("*.sublime-theme"):
+        dirname,group,theme_name=theme_path.split("/")
+        d.setdefault(group,[]).append(theme_name)
 
-    for theme in packages:
-        pkg = zipfile.ZipFile(os.path.join(INST_PKGS_PATH,theme))
-        sub_themes = list(map(parsePkgName, filter(is_theme, pkg.namelist())))
-        if sub_themes:
-            menu.append({
-                'caption': parsePkgName(theme),
-                'children': [{
-
-                    "caption":sub_theme, 
-                    "command":"switchtheme", 
-                    "args": {"name":sub_theme+".sublime-theme"}
-                    } for sub_theme in sorted(sub_themes)]
-            })
+    for theme_group in sorted(d.keys()):
+        menu.append({
+            'caption': theme_group,
+            'children': [{
+                "caption":parsePkgName(theme), 
+                "command":"switchtheme", 
+                "args": {"name":theme}
+                } 
+            for theme in sorted(d[theme_group],key=lambda x:x.replace("."," "))]
+        })
 
     menu.append({"caption": "-", "id": "separator"});    
     menu.append({"caption": "Refresh Theme Cache", "command": "refresh"})
 
     return menu
 
-def is_theme(name):
-    return "sublime-theme" == name.split('.')[-1]
 
 def parsePkgName(pkg_name):
     return pkg_name.replace("." + pkg_name.split('.')[-1],"")
    
-def plugin_loaded(): init()
-def plugin_unloaded(): os.remove(THEMES_MENU)
+def plugin_loaded(): 
+    init()
+
+def plugin_unloaded(): 
+    os.remove(THEMES_MENU)
     
 class switchthemeCommand(sublime_plugin.WindowCommand):
     def run(self,name):
-        settings = sublime.load_settings("Preferences.sublime-settings")
-        settings.set('theme', name )
+        sublime.load_settings("Preferences.sublime-settings").set('theme', name)
         sublime.save_settings("Preferences.sublime-settings")
 
 class refreshCommand(sublime_plugin.WindowCommand):
-    def run(self): init()
+    def run(self): 
+        init()
