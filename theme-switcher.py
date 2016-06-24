f61085067270940453806421e9fea54da57af9ee
DeathAxe <deathaxe@web.de>
2016-06-24 17:05:09 +0200

    a major update
    - MENU_BASE does not need to exist globally all the time.
    - Initializing PKG_FOLDER and THEMES_MENU globally does not work properly
      as sublime.packages_path() does not return a valid path on application start.
      This results in 'Theme-Switcher/Main.sublime-menu' being created in two locations,
      the application root and the package directory later.
    - Moved the content of init() directly to plugin_loaded() as there is no need for an extra function.
    - Deleting files in plugin_unloaded() should fail silently in order not to abort reloading the package.
    - In create_menu() the real package name is now used as group name for the menu items as some packages like
      Materialize.sublime-package organize their theme-files in sub directories. This results in
      stupid 'theme' menu items instead of 'Materialize'.
    - Renamed the 'refreshCommand' to a more meaningfull 'RefreshThemeMenuCommand'.
    - Add support for changing themes and color schemes with the help of the command palette by adding a Default.sublime-commands
      and extending the SwitchThemeCommand and adding SwichtColorSchemeCommand.

--- theme-switcher.py
+++ theme-switcher.py
@@ -1,79 +1,167 @@
 import sublime
 import sublime_plugin
 import os
-import sys
 import json
-import zipfile
-
-MENU_BASE = """
-[
-    {
-        "id": "preferences",
-        "children":
-        [{
-            "caption": "Theme",
-            "id": "themes"
-        }]
-    }
-]
-""".strip()
-
-PKG_FOLDER = os.path.join(sublime.packages_path(), "Theme-Switcher")
-THEMES_MENU = os.path.join(PKG_FOLDER, "Main.sublime-menu")
-
-def init():   
-    if not os.path.isdir(PKG_FOLDER):
-        os.makedirs(PKG_FOLDER)
-    
-    if not os.path.isfile(THEMES_MENU):
-        open(THEMES_MENU, "w").close()
-    
-    menu = json.loads(MENU_BASE)
-    menu[0]['children'][0]['children'] = create_menu()
-    with open(THEMES_MENU,"w") as f:
-        f.write(json.dumps(menu,indent=4))
 
 
 def create_menu():
     menu=[]
     d={}
 
-    for theme_path in sublime.find_resources("*.sublime-theme"):
-        group,theme_name=theme_path.split("/")[-2:]
-        d.setdefault(group,[]).append(theme_name)
+    for path in sublime.find_resources("*.sublime-theme"):
+        path_parts=path.split("/")
+        # path_parts[1] package name
+        # path_parts[-1] theme file name
+        d.setdefault(path_parts[1], []).append(path_parts[-1])
 
     for theme_group in sorted(d.keys()):
         menu.append({
-            'caption': theme_group,
-            'children': [{
-                "caption":parsePkgName(theme), 
-                "command":"switchtheme", 
-                "args": {"name":theme}
-                } 
-            for theme in sorted(d[theme_group],key=lambda x:x.replace("."," "))]
+            "caption": theme_group,
+            "children": [
+                {
+                    "caption": parse_pkg_name(theme),
+                    "command": "switch_theme",
+                    "args": {"name": theme}
+                }
+                for theme in sorted(d[theme_group], key=lambda x:x.replace(".", " "))
+            ]
         })
 
-    menu.append({"caption": "-", "id": "separator"});    
-    menu.append({"caption": "Refresh Theme Cache", "command": "refresh"})
+    menu.append({"caption": "-", "id": "separator"});
+    menu.append({"caption": "Refresh Theme Cache", "command": "refresh_theme_menu"})
 
     return menu
 
 
-def parsePkgName(pkg_name):
-        return pkg_name.replace(".sublime-theme","").replace("-", " ")
-   
-def plugin_loaded(): 
-    init()
+def parse_pkg_name(pkg_name):
+    return pkg_name[:pkg_name.rfind(".")].replace("-", " ").replace(".", " ")
 
-def plugin_unloaded(): 
-    os.remove(THEMES_MENU)
-    os.rmdir(PKG_FOLDER)
-    
-class switchthemeCommand(sublime_plugin.WindowCommand):
-    def run(self,name):
-        sublime.load_settings("Preferences.sublime-settings").set('theme', name)
+
+def plugin_loaded():
+    """
+        Refresh cached Main.sublime-menu if plugin is loaded.
+    """
+    global PKG_FOLDER, THEMES_MENU
+
+    PKG_FOLDER=os.path.join(sublime.packages_path(), "User/Theme-Switcher.cache")
+    THEMES_MENU=os.path.join(PKG_FOLDER, "Main.sublime-menu")
+
+    if not os.path.isdir(PKG_FOLDER):
+        os.makedirs(PKG_FOLDER)
+
+    menu=json.loads("""
+                    [{
+                        "id": "preferences",
+                        "children":
+                        [{
+                            "caption": "Theme",
+                            "id": "themes"
+                        }]
+                    }]
+                    """)
+    menu[0]['children'][0]['children']=create_menu()
+    with open(THEMES_MENU, "w") as f:
+        f.write(json.dumps(menu, indent=4))
+
+
+def plugin_unloaded():
+    """
+        Clear cached Main.sublime-menu if the plugin is disabled or removed.
+    """
+    global PKG_FOLDER, THEMES_MENU
+
+    try:
+        os.remove(THEMES_MENU)
+    except:
+        pass
+
+    try:
+        os.rmdir(PKG_FOLDER)
+    except:
+        pass
+
+
+class RefreshThemeMenuCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        plugin_loaded()
+
+
+class SwitchCommandBase(sublime_plugin.WindowCommand):
+    """
+        This is a base class for a window command to apply changes to
+        the Preferences.sublime-settings.
+
+        It is used to switch between all available themes and color schemens
+        with the help of the quick panel.
+    """
+
+    _items = [ [], [] ]
+
+    def run(self, name=None):
+        """
+            This command has two functions.
+            1. If no <name> is provided, it shows a quick panel on the active
+               window with all available themes or color schemes.
+            2. If <name> is a valid string, the provided theme or color scheme is applied.
+        """
+        if name:
+            self.apply(name)
+        else:
+            self._items = [ [], [] ]
+            self.get_items()
+            self.window.show_quick_panel(self._items[0], self.on_select)
+
+    def on_select(self, index):
+        if -1 < index < len(self._items[1]):
+            self.apply(self._items[1][index])
+
+    def apply(self, name):
+        sublime.load_settings("Preferences.sublime-settings").set(self.get_key(), name)
         sublime.save_settings("Preferences.sublime-settings")
 
-class refreshCommand(sublime_plugin.WindowCommand):
-    def run(self): 
-        init()
+
+class SwitchThemeCommand(SwitchCommandBase):
+
+    def get_key(self):
+        """
+            Return the key in the Preferences.sublime-settings
+            to manipulate by this command.
+        """
+        return 'theme'
+
+    def get_items(self):
+        """
+            Return a list of all available themes to show in the quick panel
+            with the following format:
+            1. title (_items[0]) to show in the list and
+            2. values (_items[1]) to write to the Preferences.sublime-settings
+
+            In this case the values are the basename of each *.sublime-theme.
+        """
+        for path in sorted(sublime.find_resources("*.sublime-theme")):
+            path = os.path.basename(path)
+            self._items[0].append(parse_pkg_name(path))
+            self._items[1].append(path)
+
+
+class SwitchColorSchemeCommand(SwitchCommandBase):
+
+    def get_key(self):
+        """
+            Return the key in the Preferences.sublime-settings
+            to manipulate by this command.
+        """
+        return 'color_scheme'
+
+    def get_items(self):
+        """
+            Return a list of all available color schemes to show in the quick panel
+            with the following format:
+            1. title (_items[0]) to show in the list and
+            2. values (_items[1]) to write to the Preferences.sublime-settings
+
+            In this case the values are the full path of each *.tmTheme.
+        """
+        for path in sorted(sublime.find_resources("*.tmTheme")):
+            self._items[0].append(parse_pkg_name(os.path.basename(path)))
+            self._items[1].append(path)
