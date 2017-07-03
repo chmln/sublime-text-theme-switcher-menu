b17b05e151658e5140a458435ce23171a59981e7
deathaxe <deathaxe@web.de>
2017-07-03 14:51:02 +0200

    A set of enhencements (#6)
    * Select default color scheme or theme if nothing else is set
    
    If the initial color scheme or theme can't be read from settings object, use safe defaults.
    
    * Debounce color scheme / theme preview
    
    Don't update preview on each key press but after 250ms so user can quickly scroll through the list of items without changing color scheme too often.
    
    * Sort quick panel items case insensitive and strip multiple whitespace from name
    
    If built_res_name replaced a " - " (e.g.: Atomic Color Schemes) the returned name contains multiple (3) whitespace which don't look nice.
    
    * Apply color scheme preview to all visible views.
    
    This feature is inspired by ST's default ui.py.
    
    1. Views with project or view specific color schemes are detected.
    2. The highlighted color scheme is applied to them for preview.
    3. If quick panel closes, the view specific color schemes are restored.
    
    Without this feature, views with view specific color schemes don't display the preview.
    
    * Avoid duplicated theme items.
    
    Themes are merged by ST. Therefore only the first found theme is displayed if several are found with same file name.
    Otherwise a theme in the User path, which is used to overide a couple of theme rules would be displayed in the list.

--- theme_switcher.py
+++ theme_switcher.py
@@ -11,7 +11,7 @@ def menu_cache_path():
 def built_res_name(pkg_name):
     """Built menu or quick panel item name from a resource file name."""
     name, _ = os.path.splitext(os.path.basename(pkg_name))
-    return name.replace("-", " ").replace(".", " ")
+    return name.replace(" - ", " ").replace("-", " ").replace(".", " ")
 
 
 def plugin_loaded():
@@ -81,6 +81,18 @@ class RefreshThemeCacheCommand(sublime_plugin.ApplicationCommand):
 class SwitchWindowCommandBase(sublime_plugin.WindowCommand):
     """Base class for SwitchThemeCommand and SwitchColorSchemeCommand."""
 
+    PREFS_FILE = 'Preferences.sublime-settings'
+
+    # A set of tuples (settings, original) with view specific color schemes or
+    # themes which need to be restored after all as this command intends to
+    # apply the global color scheme or theme only but applies preview to all
+    # views no matter they use their own settings or not.
+    overridden = set()
+
+    # The last selected row index - used to debounce the search so we
+    # aren't apply a new color scheme or theme with every keypress
+    last_index = -1
+
     def run(self, name=None):
         """API entry point for command execution."""
         if name:
@@ -91,37 +103,67 @@ class SwitchWindowCommandBase(sublime_plugin.WindowCommand):
     def apply_setting(self, name):
         """Directly apply the provided theme or color scheme."""
         if any(sublime.find_resources(os.path.basename(name))):
-            settings_file = "Preferences.sublime-settings"
-            sublime.load_settings(settings_file).set(self.KEY, name)
-            sublime.save_settings(settings_file)
+            sublime.load_settings(self.PREFS_FILE).set(self.KEY, name)
+            sublime.save_settings(self.PREFS_FILE)
         else:
             sublime.status_message(name + " does not exist!")
 
     def show_quick_panel(self):
         """List all available themes or color schemes in a quick panel."""
-        settings_file = "Preferences.sublime-settings"
-        settings = sublime.load_settings(settings_file)
+        settings = sublime.load_settings(self.PREFS_FILE)
         names, values = self.get_items()
-        current_value = settings.get(self.KEY)
+        current_value = settings.get(self.KEY, self.DEFAULT)
         selected_index = self.get_selected(values, current_value)
 
         def on_select(index):
+            # reset all view specific settings to initial values
+            self.reset_overridden()
+            # apply global settings
             if -1 < index < len(values):
                 settings.set(self.KEY, values[index])
-            elif current_value:
+            else:
                 settings.set(self.KEY, current_value)
-            sublime.save_settings(settings_file)
+            sublime.save_settings(self.PREFS_FILE)
 
         def on_highlight(index):
-            settings.set(self.KEY, values[index])
+            if index == -1:
+                return
+
+            self.last_index = index
+
+            def update_ui():
+                if index != self.last_index:
+                    return
+                value = values[index]
+                if settings.get(self.KEY) == value:
+                    return
+                # apply value to global settings
+                settings.set(self.KEY, value)
+                # apply value to overridden views
+                if not self.overridden:
+                    self.find_overridden(value)
+                for view_setting, _ in self.overridden:
+                    view_setting.set(self.KEY, value)
+
+            sublime.set_timeout(update_ui, 250)
 
         self.window.show_quick_panel(
             items=names, selected_index=selected_index,
             on_highlight=on_highlight, on_select=on_select)
 
+    def find_overridden(self, global_value):
+        """Find view specific settings and their initial values."""
+        return None
+
+    def reset_overridden(self):
+        """Reset view specific settings to their initial values."""
+        self.overridden.clear()
+
 
 class SwitchThemeCommand(SwitchWindowCommandBase):
+
     KEY = 'theme'
+    DEFAULT = 'Default.sublime-theme'
 
     @staticmethod
     def get_items():
@@ -135,19 +177,23 @@ class SwitchThemeCommand(SwitchWindowCommandBase):
         names = []
         values = []
         settings = sublime.load_settings("Theme-Switcher.sublime-settings")
-        exclude_list = settings.get("themes_exclude", [])
+        exclude_list = settings.get("themes_exclude") or []
         paths = sorted(
             sublime.find_resources("*.sublime-theme"),
-            key=lambda x: os.path.basename(x))
+            key=lambda x: os.path.basename(x).lower())
         for path in paths:
             if not any(exclude in path for exclude in exclude_list):
-                elems = path.split("/")
-                # elems[1] package name
-                # elems[-1] theme file name
+                parts = path.split("/")
+                theme = parts[-1]
+                # themes are merged by ST so display only first one
+                if theme in values:
+                    continue
+                # parts[1] package name
+                # parts[-1] theme file name
                 names.append(
-                    [built_res_name(elems[-1]),    # title
-                     "Package: " + elems[1]])      # description
-                values.append(elems[-1])
+                    [built_res_name(theme),       # title
+                     "Package: " + parts[1]])     # description
+                values.append(theme)
         return [names, values]
 
     @staticmethod
@@ -161,7 +207,9 @@ class SwitchThemeCommand(SwitchWindowCommandBase):
 
 
 class SwitchColorSchemeCommand(SwitchWindowCommandBase):
+
     KEY = 'color_scheme'
+    DEFAULT = 'Packages/Color Scheme - Default/Monokai.tmTheme'
 
     @staticmethod
     def get_items():
@@ -175,10 +223,10 @@ class SwitchColorSchemeCommand(SwitchWindowCommandBase):
         names = []
         values = []
         settings = sublime.load_settings("Theme-Switcher.sublime-settings")
-        exclude_list = settings.get("colors_exclude", [])
+        exclude_list = settings.get("colors_exclude") or []
         paths = sorted(
             sublime.find_resources("*.tmTheme"),
-            key=lambda x: os.path.basename(x))
+            key=lambda x: os.path.basename(x).lower())
         for path in paths:
             if not any(exclude in path for exclude in exclude_list):
                 elems = path.split("/")
@@ -210,3 +258,43 @@ class SwitchColorSchemeCommand(SwitchWindowCommandBase):
             except ValueError:
                 selected_index = -1
         return selected_index
+
+    def find_overridden(self, global_cs):
+        """Create a set of view specific color schemes and their defaults.
+
+        Arguments:
+            global_cs (string):
+                the global color scheme from Preferences.sublime-settings
+
+        Returns:
+            tuple (settings, original)
+             - settings (sublime.Settings): the view settings object
+             - original (string): the original "color_scheme" for the view
+        """
+        project = self.window.project_data()
+        pcs = project.get('settings', {}).get(self.KEY) if project else None
+        for i in range(self.window.num_groups()):
+            view = self.window.active_view_in_group(i)
+            view_settings = view.settings()
+            vcs = view_settings.get(self.KEY, self.DEFAULT)
+            if pcs is not None:
+                # view shows project specific setting
+                # needs to be deleted after all
+                if vcs == pcs and pcs != global_cs:
+                    self.overridden.add((view_settings, None))
+                    continue
+            if vcs != global_cs:
+                # view shows view specific setting
+                # needs to be reset to default value after all
+                self.overridden.add((view_settings, vcs))
+
+    def reset_overridden(self):
+        """Reset view specific settings to their initial values."""
+        for settings, original in self.overridden:
+            if original is None:
+                # apply project specific values
+                settings.erase(self.KEY)
+            else:
+                # apply view specific values
+                settings.set(self.KEY, original)
+        super().reset_overridden()
